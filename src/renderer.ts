11c0839d5bd8d30eb7bb86eb160439be9d526907
道化師 <wraith13@hotmail.co.jp>
2019-02-05 22:00:53 +0900

    🛠 minamo.js の組み込み

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,4 +2,7 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 
+import { minamo } from "./minamo";
+
 document.write("🐕");
+minamo.dom.appendChildren(document.body, { tag: "p", children: "Hello, minamo.js!"});
