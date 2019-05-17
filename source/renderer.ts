e9ddc4950be02917e757de820831c7057ce0f573
道化師 <wraith13@hotmail.co.jp>
2019-05-17 21:33:44 +0900

    🛠 実装( WIP )

--- source/renderer.ts
+++ source/renderer.ts
@@ -3,6 +3,17 @@
 // All of the Node.js APIs are available in this process.
 
 import { minamo } from "./minamo";
+import * as fs from 'fs';
 
 document.write("🐕");
 minamo.dom.appendChildren(document.body, { tag: "p", children: "Hello, minamo.js!"});
+
+fs.readdir
+(
+    "/",
+    (_err, files) => minamo.dom.appendChildren
+    (
+        document.body,
+        files.map(i => ({ tag: "p", children: i}))
+    )
+);
