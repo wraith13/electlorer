db91cf797e78efba3badfe305557edc37051bf38
道化師 <wraith13@hotmail.co.jp>
2019-05-19 03:01:42 +0900

    🛠 実装( WIP )

--- source/renderer.ts
+++ source/renderer.ts
@@ -5,21 +5,64 @@
 import { minamo } from "./minamo";
 import * as fs from 'fs';
 
+module fx
+{
+    export function readdir(path : string)
+        : Promise<{ error : NodeJS.ErrnoException, files : string[] }>
+    {
+        return new Promise
+        (
+            resolve => fs.readdir
+            (
+                path,
+                (error : NodeJS.ErrnoException, files : string[]) => resolve
+                (
+                    {
+                        error,
+                        files
+                    }
+                )
+            )
+        );
+    }
+
+    export function exists(path : string) : Promise<boolean>
+    {
+        return new Promise
+        (
+            resolve => fs.exists
+            (
+                path,
+                exists => resolve(exists)
+            )
+        );
+    }
+
+    export function readFile(path : string)
+        : Promise<{ err : NodeJS.ErrnoException, data : Buffer }>
+    {
+        return new Promise
+        (
+            resolve => fs.readFile
+            (
+                path,
+                (err : NodeJS.ErrnoException, data : Buffer) => resolve({ err, data })
+            )
+        );
+    }
+}
+
 document.write("🐕");
 minamo.dom.appendChildren(document.body, { tag: "p", children: "Hello, minamo.js!"});
 
-fs.readdir
+const readDir = async (parent: Element, path: string) => minamo.dom.appendChildren
 (
-    "/",
-    (_err, files) =>
-    (
-        {
-            tag: "ul",
-            children: minamo.dom.appendChildren
-            (
-                document.body,
-                files.map(i => ({ tag: "li", children: i}))
-            )
-        }
-    )
+    parent,
+    {
+        tag: "ul",
+        children: (await fx.readdir(path)).files.map(i => ({ tag: "li", children: i}))
+    }
 );
+
+readDir(document.body, "/");
+
